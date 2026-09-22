03cfecda10001b4a56ebe8116192ae45d8757501
Ayesha Bhatt <97374792+itsaisha17@users.noreply.github.com>
2026-09-23 01:10:56 +0530

    Time: 624 ms (87.64%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,12 +1,9 @@
-#delivery date= order date-> immediate ; else scheduled
-#first order-> earliest order date
-# %immediate orders in first order 
-
-select 
-ROUND(AVG(order_date=customer_pref_delivery_date)*100,2) as immediate_percentage
+Select  
+ROUND(AVG(order_date= customer_pref_delivery_date)*100,2)
+as immediate_percentage
 from Delivery
-where (customer_id,order_date) IN(
-    select customer_id, min(order_date) 
-    from delivery
+where (customer_id,order_date) IN (
+    select customer_id, min(order_date)
+    from Delivery
     group by customer_id
 )
